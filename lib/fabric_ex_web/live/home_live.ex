eef2623b59834626e6db4939a6e9ee12c19a6f4f
Wendy Randolph <wendy.randolph5@gmail.com>
2024-03-06 21:51:09 -0500

    Add table view of fabrics on home page

--- lib/fabric_ex_web/live/home_live.ex
+++ lib/fabric_ex_web/live/home_live.ex
@@ -5,6 +5,94 @@ defmodule FabricExWeb.HomeLive do
   def render(assigns) do
       ~H"""
       <h1 class="text-2xl">Fabric Stash</h1>
+
+<div class="overflow-x-auto">
+  <table class="table">
+    <!-- head -->
+    <thead>
+      <tr>
+        <th>image</th>
+        <th>yards</th>
+        <th>shade</th>
+        <th>color</th>
+        <th>weight</th>
+  	    <th>structure</th>
+        <th>content</th>
+        <th>width</th>
+      </tr>
+    </thead>
+    <tbody>
+      <!-- row 1 -->
+      <tr>
+        <th>
+        <div class="flex items-center gap-3">
+          <div class="fabric photo">
+            <img src="https://fabrics-store.com/images/product/FS_F_19231680272071_1000x1000.jpg" alt="grey" />
+          </div>
+        </div>
+        </th>
+        <td>1</td>
+        <td>medium</td>
+        <td>grey</td>
+        <th>midweight</th>
+        <td>woven</td>
+        <td>linen</td>
+        <th>59"</th>
+      </tr>
+      <!-- row 2 -->
+      <tr>
+        <th>
+        <div class="flex items-center gap-3">
+          <div class="fabric photo">
+            <img src="https://fabrics-store.com/images/product/FS_F_19231680272071_1000x1000.jpg" alt="grey" />
+          </div>
+        </div>
+        </th>
+        <td>1</td>
+        <td>medium</td>
+        <td>grey</td>
+        <th>midweight</th>
+        <td>woven</td>
+        <td>linen</td>
+        <th>59"</th>
+      </tr>
+      <!-- row 3 -->
+        <tr>
+        <th>
+        <div class="flex items-center gap-3">
+          <div class="fabric photo">
+            <img src="https://fabrics-store.com/images/product/FS_F_19231680272071_1000x1000.jpg" alt="grey" />
+          </div>
+        </div>
+        </th>
+        <td>1</td>
+        <td>medium</td>
+        <td>grey</td>
+        <th>midweight</th>
+        <td>woven</td>
+        <td>linen</td>
+        <th>59"</th>
+      </tr>
+      <!-- row 4 -->
+        <tr>
+        <th>
+        <div class="flex items-center gap-3">
+          <div class="fabric photo">
+            <img src="https://fabrics-store.com/images/product/FS_F_19231680272071_1000x1000.jpg" alt="grey" />
+          </div>
+        </div>
+        </th>
+        <td>1</td>
+        <td>medium</td>
+        <td>grey</td>
+        <th>midweight</th>
+        <td>woven</td>
+        <td>linen</td>
+        <th>59"</th>
+      </tr>
+    </tbody>
+  </table>
+</div>
       """
   end
 
