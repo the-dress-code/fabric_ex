904265f5826066707bc1dd9a669a435492c1193e
Wendy Randolph <wendy.randolph5@gmail.com>
2024-03-08 12:21:18 -0500

    Fixed indentation

--- lib/fabric_ex_web/live/home_live.ex
+++ lib/fabric_ex_web/live/home_live.ex
@@ -6,93 +6,59 @@ defmodule FabricExWeb.HomeLive do
       ~H"""
       <h1 class="text-2xl">Fabric Stash</h1>
 
-<div class="overflow-x-auto">
-  <table class="table">
-    <!-- head -->
-    <thead>
-      <tr>
-        <th>image</th>
-        <th>yards</th>
-        <th>shade</th>
-        <th>color</th>
-        <th>weight</th>
-  	    <th>structure</th>
-        <th>content</th>
-        <th>width</th>
-      </tr>
-    </thead>
-    <tbody>
-      <!-- row 1 -->
-      <tr>
-        <th>
-        <div class="flex items-center gap-3">
-          <div class="fabric photo">
-            <img src="https://fabrics-store.com/images/product/FS_F_19231680272071_1000x1000.jpg" alt="grey" />
-          </div>
-        </div>
-        </th>
-        <td>1</td>
-        <td>medium</td>
-        <td>grey</td>
-        <th>midweight</th>
-        <td>woven</td>
-        <td>linen</td>
-        <th>59"</th>
-      </tr>
-      <!-- row 2 -->
-      <tr>
-        <th>
-        <div class="flex items-center gap-3">
-          <div class="fabric photo">
-            <img src="https://fabrics-store.com/images/product/FS_F_19231680272071_1000x1000.jpg" alt="grey" />
-          </div>
-        </div>
-        </th>
-        <td>1</td>
-        <td>medium</td>
-        <td>grey</td>
-        <th>midweight</th>
-        <td>woven</td>
-        <td>linen</td>
-        <th>59"</th>
-      </tr>
-      <!-- row 3 -->
+      <div class="overflow-x-auto">
+        <table class="table">
+        <!-- head -->
+          <thead>
+            <tr>
+              <th>image</th>
+              <th>yards</th>
+              <th>shade</th>
+              <th>color</th>
+              <th>weight</th>
+              <th>structure</th>
+              <th>content</th>
+              <th>width</th>
+            </tr>
+          </thead>
+        <tbody>
+        <!-- row 1 -->
         <tr>
-        <th>
-        <div class="flex items-center gap-3">
-          <div class="fabric photo">
-            <img src="https://fabrics-store.com/images/product/FS_F_19231680272071_1000x1000.jpg" alt="grey" />
-          </div>
-        </div>
-        </th>
-        <td>1</td>
-        <td>medium</td>
-        <td>grey</td>
-        <th>midweight</th>
-        <td>woven</td>
-        <td>linen</td>
-        <th>59"</th>
-      </tr>
-      <!-- row 4 -->
+          <th>
+            <div class="flex items-center gap-3">
+            <div class="fabric photo">
+              <img src="https://fabrics-store.com/images/product/FS_F_19231680272071_1000x1000.jpg" alt="grey" />
+            </div>
+            </div>
+          </th>
+          <td>1</td>
+          <td>medium</td>
+          <td>grey</td>
+          <td>midweight</td>
+          <td>woven</td>
+          <td>linen</td>
+          <td>59"</td>
+        </tr>
+        <!-- row 2 -->
         <tr>
-        <th>
-        <div class="flex items-center gap-3">
-          <div class="fabric photo">
-            <img src="https://fabrics-store.com/images/product/FS_F_19231680272071_1000x1000.jpg" alt="grey" />
+          <th>
+            <div class="flex items-center gap-3">
+            <div class="fabric photo">
+              <img src="https://fabrics-store.com/images/product/FS_F_19231680272071_1000x1000.jpg" alt="grey" />
+            </div>
           </div>
-        </div>
-        </th>
-        <td>1</td>
-        <td>medium</td>
-        <td>grey</td>
-        <th>midweight</th>
-        <td>woven</td>
-        <td>linen</td>
-        <th>59"</th>
-      </tr>
-    </tbody>
-  </table>
-</div>
+          </th>
+          <td>1</td>
+          <td>medium</td>
+          <td>grey</td>
+          <td>midweight</td>
+          <td>woven</td>
+          <td>linen</td>
+          <td>59"</td>
+        </tr>
+        </tbody>
+      </table>
+      </div>
       """
   end
 
